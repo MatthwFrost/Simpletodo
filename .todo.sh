49731c00f0a29a1ff3ac0ef2606a431fe488f6fc
matthwfrost <matthwfrost@gmail.com>
2022-05-11 03:04:39 +0100

    Fixed some bugs

--- .todo.sh
+++ .todo.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-# Add support to input text into different files *
+# Add support to input text into different files
+# Make the path the name of the todo file
 
 
 # Name your todo files, Please avoid spaces
-NAME="todo.md"
+NAME="todo"
 
 
 # The text you see at the top of your todo file.
-TITLE="New Todo List"
+TITLE="Todo List"
 
 
 # Checks if bash is being used
 if [ -z "${BASH_VERSION:-}" ]
 then
-    abort "Bssh is needed for this script"
+    abort "Bash is needed for this script"
 fi
 
 
